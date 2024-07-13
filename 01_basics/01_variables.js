d20a4a735c960d445fa609cd174513cf848cbdda
shruti singh parihar <138442827+parihar200@users.noreply.github.com>
2024-07-13 09:12:46 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 1054456;
+let accountEmail="shrut@google.com"
+var accountNumber="1234"
+accountCity="Bhopal"
+accountState =null;
+// accountId =4
+accountEmail="tw@GOOGLE.COM"
+accountNumber=459752
+accountCity="indore"
+
+console.log("test",accountId)
+console.table([accountEmail,accountNumber,accountCity,accountState])
+/*
+Prefer Not to use Var
+because of issue in block scope
+and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict" //treat all js code as newer version
+
+alert(1+2); //We are using node js , not browser
