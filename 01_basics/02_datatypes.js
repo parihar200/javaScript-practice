09e365d8ce4864e8134754f7932cc70b43ec6277
shruti singh parihar <138442827+parihar200@users.noreply.github.com>
2024-07-15 10:55:42 +0000

    conversion opration

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,19 @@
 "use strict" //treat all js code as newer version
+let name="shruti"
+let age=23
+let loggedIn=false
+let state
+// alert(1+2); //We are using node js , not browser
+// console.log(1+3)
+// number => 2 to power 53
+// BigInt 
+// String => ""
+// Boolean => true/false
+// null => standalon value
+// undefined
+// Symbol =>unique
 
-alert(1+2); //We are using node js , not browser
+
+// Object
+console.log(typeof null) //object type
+console.log(typeof undefined) //object undefined
---
+++ 01_basics/conversionopration.js
@@ -0,0 +1,37 @@
+let score = "44fd";
+// console.log(typeof score);
+// console.log(typeof (score));
+let valuenumber = Number(score)
+// console.log(valuenumber)
+// console.log(typeof valuenumber )
+// console.log(valuenumber)
+
+// "33" => 33
+// "44sdf"=>NaN
+// true => 1; false=>0 
+
+let isloggedIn=1
+let loggedIn =Boolean(isloggedIn)
+// console.log(loggedIn)
+
+// 1=>true 
+// ""=>false
+// "dfg"=>true
+
+let someString=78;
+let stringValue =String(someString);
+// console.log( stringValue);
+
+// ********************************oprations***********************************************
+let num = "str"
+let num2 ="pr"
+// console.log(num+num2)
+
+// console.log(3+"4")
+// console.log("3"+6)
+console.log("d"+7+6)
+console.log(7+6+"d")
+
+
+
+
